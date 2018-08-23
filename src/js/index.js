dfabd5107949436701c6cb06323afd6cdb455998
runandrerun <the.asantiagojr@gmail.com>
2018-08-23 16:10:52 -0400

    frontend

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   const chatWebSocket = openConnection()
+  const userWebSocket = openConnection()
+
+  userWebSocket.onopen = (event) => {
+    const subscribeUser = {"command":"subscribe","identifier":"{\"channel\":\"UsersOnlineChannel\"}"}
+    userWebSocket.send(JSON.stringify(subscribeUser))
+  }
 
   chatWebSocket.onopen = (event) => {
     const subscribeMsg = {"command":"subscribe","identifier":"{\"channel\":\"ChatMessagesChannel\"}"}
 
     chatWebSocket.send(JSON.stringify(subscribeMsg))
   }
-  // const currentUser = document.getElementById('user-name')
-  // const liUser = document.getElementById('')
+
   const onlineList = document.getElementById('online-list')
   const chatContent = document.querySelector("#chat-content")
   chatWebSocket.onmessage = event => {
@@ -29,8 +34,17 @@ document.addEventListener("DOMContentLoaded", () => {
     scrollDown()
   }
 
+  userWebSocket.onmessage = event => {
+    const result = JSON.parse(event.data)
+    if (result["message"]["users"]) {
+      const user = result["message"]["users"]
+      const onlineNow = document.createElement('li')
+      onlineNow.id = user
+      onlineNow.innerHTML = user
+      onlineList.append(onlineNow)
+    }
+  }
 
-  // ONLINE NOW LIST
 
 
   const chatField = document.querySelector("#chat-field")
@@ -49,39 +63,40 @@ document.addEventListener("DOMContentLoaded", () => {
   const userName = document.getElementById('user-field')
   const userBtn = document.getElementById('submitUser')
   userBtn.onclick = () => {
-    // const msg = {"command":"message","identifier":"{\"channel\":\"ChatMessagesChannel\"}","data":`{\"action\": \"user\", \"username\": \"${userName.value}\" }`}
-    // chatWebSocket.send(JSON.stringify(msg))
+    event.preventDefault()
 
     document.getElementById('userForm').innerHTML = ''
     document.getElementById('user-name').innerHTML = `Logged in as: ${userName.value}`
 
-    const welcomeMessage = document.createElement("p")
-    const onlineNow = document.createElement('li')
-    onlineNow.id = sessionStorage.getItem('username')
-    onlineList.innerHTML = ''
-    onlineNow.innerHTML = userName.value
-    welcomeMessage.innerText = `${userName.value} has logged in!`
-    onlineList.append(onlineNow)
-    chatContent.append(welcomeMessage)
+    // const welcomeMessage = document.createElement("p")
+    // const onlineNow = document.createElement('li')
+    // onlineNow.id = sessionStorage.getItem('username')
+    // onlineList.innerHTML = ''
+    // onlineNow.innerHTML = userName.value
+    // welcomeMessage.innerText = `${userName.value} has logged in!`
+    // onlineList.append(onlineNow)
+    // chatContent.append(welcomeMessage)
 
     const currentUser = sessionStorage.setItem('username', userName.value)
+
+    const msg = {"command":"message","identifier":"{\"channel\":\"UsersOnlineChannel\"}","data":`{\"action\": \"user\", \"username\": \"${sessionStorage.getItem('username')}\" }`}
+    chatWebSocket.send(JSON.stringify(msg))
+
     const logoutBtn = document.createElement('button')
     logoutBtn.innerText = "Logout"
     logoutBtn.className = "btn btn-danger"
     logoutBtn.onclick = () => {
+      const onlineUser = document.getElementById(`${sessionStorage.getItem('username')}`)
       sessionStorage.clear();
-      onlineNow.innerHTML = ''
+      onlineUser.innerHTML = ''
       currentU.innerHTML = ''
-      onlineList.innerHTML = ''
+      onlineList.innerHTML = "You are not logged in!"
       const h = document.createElement('h4')
       h.innerText = 'Logged Out!'
       currentU.append(h)
-
     }
     currentU.append(logoutBtn)
-
   }
-
 })
 
 function scrollDown() {
