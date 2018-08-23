9d1061ac98768881458db374e3632064312fe6b4
Dan Chung <danetix@gmail.com>
2018-08-23 10:30:21 -0400

    reverted heroku test settings

--- src/js/adapter.js
+++ src/js/adapter.js
@@ -1,4 +1,4 @@
 function openConnection() {
-  // return new WebSocket("ws://localhost:3000/cable")
-  return new WebSocket("wss://flatironchatterbox-api.herokuapp.com/cable")
+  return new WebSocket("ws://localhost:3000/cable")
+  // return new WebSocket("wss://flatironchatterbox-api.herokuapp.com/cable")
 }
