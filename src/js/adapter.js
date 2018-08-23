62f7a6051ac41d1f9654cc3db1d26785845f4daf
Dan Chung <danetix@gmail.com>
2018-08-23 10:11:09 -0400

    trying wss

--- src/js/adapter.js
+++ src/js/adapter.js
@@ -1,4 +1,4 @@
 function openConnection() {
   // return new WebSocket("ws://localhost:3000/cable")
-  return new WebSocket("ws://flatironchatterbox.herokuapp.com/cable")
+  return new WebSocket("wss://flatironchatterbox.herokuapp.com/cable")
 }
