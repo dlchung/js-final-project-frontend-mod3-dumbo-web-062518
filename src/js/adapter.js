a8b318396f37339424e84dc6c97152f7ef014ef9
Dan Chung <danetix@gmail.com>
2018-08-25 16:59:07 -0400

    remote test

--- src/js/adapter.js
+++ src/js/adapter.js
@@ -1,5 +1,5 @@
 function openConnection() {
-  return new WebSocket("ws://localhost:3000/cable")
+  // return new WebSocket("ws://localhost:3000/cable")
   // return new WebSocket("ws://10.39.104.225:3000/cable")
-  // return new WebSocket("wss://flatironchatterbox-api.herokuapp.com/cable")
+  return new WebSocket("wss://flatironchatterbox-api.herokuapp.com/cable")
 }
